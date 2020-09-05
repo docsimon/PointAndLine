8b8de76ed134df0b647809e589c1d7c8851183ce
doc <simone.milano12@gmail.com>
2020-09-05 16:49:43 +0100

    Added StorageApi

---
+++ PointAndLine/Model/StorageApi.swift
@@ -0,0 +1,24 @@
+////  StorageApi.swift
+//  PointAndLine
+//
+//  Created by Simone Barbara on 05/09/2020.
+//  Copyright © 2020 Simone Barbara. All rights reserved.
+//
+
+import Foundation
+
+struct StorageApi: StorageApiProtocol {
+    func save(graph: [Line], result: (Result<Bool, Error>) -> Void) {
+        
+    }
+    
+    func save(line: Line, result: (Result<Bool, Error>) -> Void) {
+        
+    }
+    
+    func load() -> [Line] {
+       return []
+    }
+    
+    
+}
